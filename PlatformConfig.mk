7cb52c74f656b34ba41376dac6a384c3f5ce9143
oshmoun <oshmoun100@googlemail.com>
2018-09-23 21:45:53 +0200

    PlatformConfig: declare brcmfmac as wifi driver
    Declare this to allow macaddrsetup to choose the proper handling to set MAC address for wifi.
    
    Change-Id: I76efd6c6a597f3f7f471be77ab57edad1b7191a5

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -58,6 +58,7 @@ BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_BUILT           := brcmfmac
 
 # BT definitions for Broadcom solution
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(PLATFORM_COMMON_PATH)/bluetooth
